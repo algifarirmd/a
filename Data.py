0d1132f0b5fbf46207bd09223d5a8eed7c686f0e
algifarirmd <111198837+algifarirmd@users.noreply.github.com>
2022-10-14 20:29:22 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,24 +6,13 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
+<b>Perintah untuk Pengguna BOT 🤠
  ├ /start - Mulai Bot
  ├ /about - Tentang Bot ini
  ├ /help - Bantuan Perintah Bot ini
  ├ /ping - Untuk mengecek bot hidup
  └ /uptime - Untuk melihat status bot 
- 
- ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
-
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+ </a>
 """
 
     close = [
@@ -48,10 +37,5 @@ class Data:
 <b>Tentang Bot ini:
 
 @{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
-
- • Creator: @{}
- • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
-
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+</a>
 """
